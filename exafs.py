1cd7c3d85b36f10b9c63f144f16881c7befaa39d
Galen O'Neil <galen.oneil@nist.gov>
2014-06-23 15:01:09 -0600

    progress towards phase_correct

--- exafs.py
+++ exafs.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pylab as plt
 import mass
+import pulse_timing
 
 basic_cuts = mass.core.controller.AnalysisControl(
     pulse_average=(0.0, None),
@@ -30,5 +31,82 @@ def drift_correct(data):
 def phase_correct(data,typical_resolution):
     # note that typical resolution must be in units of p_pulse_rms
     for ds in data:
-        ds.phase_correct2014(typical_resolution)
+        ds.phase_correct2014(typical_resolution, plot=True)
 
+def phase_correct2014_dataset(self, typical_resolution, plot=False):
+    """Apply the phase correction that seems good for calibronium-like
+    data as of June 2014. For more notes, do
+    help(mass.core.analysis_algorithms.FilterTimeCorrection)
+
+    <typical_resolution> should be an estimated energy resolution in UNITS OF
+    self.p_pulse_rms. This helps the peak-finding (clustering) algorithm decide
+    which pulses go together into a single peak.  Be careful to use a semi-reasonable
+    quantity here.
+
+    This version is for when segment 0 doesn't have enough data to train one,
+    for example when only 3% of the data is calibration data.
+    """
+    data,g = good_pulses_data(self, max_records = 20000)
+    prompt = self.p_promptness
+    dataFilter = self.filter.filt_noconst
+    tc = mass.core.analysis_algorithms.FilterTimeCorrection(
+            data, prompt[g], self.p_pulse_rms[g], dataFilter,
+            self.nPresamples, typicalResolution=typical_resolution)
+
+    self.p_filt_value_phc = self.p_filt_value_dc - tc(prompt, self.p_pulse_rms)
+
+    if plot:
+        plt.clf()
+        g = self.cuts.good()
+        plt.plot(prompt[g], self.p_filt_value_dc[g], 'g.',label="dc")
+        plt.plot(prompt[g], self.p_filt_value_phc[g], 'b.',label="phc")
+        plt.legend()
+
+def good_pulses_data(ds, max_records=20000):
+    """
+    :param ds: at dataset object
+    :param max_records: roughly maximum records to includes, it can exceed this number by segment_size
+    :return: data, g
+    data is a (X,Y) array where X is number of records, and Y is number of samples per record
+    g is a 1d array of booleans of size X
+    if we could did load all of ds.data at once, this would be roughly equivalent to
+    return ds.data[ds.cuts.good()], ds.cuts.good()
+    """
+    first, end = ds.pulse_records.read_segment(0)
+    g = ds.cuts.good()
+    data = ds.data[g[first:end]]
+    for j in xrange(1, ds.pulse_records.n_segments):
+        first, end = ds.pulse_records.read_segment(j)
+        data = np.vstack((data, ds.data[g[first:end]]))
+        if data.shape[0]>max_records:
+            break
+    return data, g[:end]
+
+def calc_laser_phase(data):
+    ds = data.first_good_dataset
+    phase, spline = pulse_timing.calc_phase(ds.p_timestamp)
+    for ds in data:
+        if not hasattr(ds, "p_laser_phase"):
+            ds.p_laser_phase = spline.phase(ds.p_timestamp)
+
+def choose_laser(data, band, cut_lines=[0.47,0.515]):
+    """
+    uses the dataset.cuts object to mark bad all pulses not in a specific category related
+    to laser timing
+    :param data: a microcal TESChannelGroup object
+    :param band: options: (1,2,"laser", "not_laser") for( band1, band2, band1 and band2, not_laser pulses)
+    :param cut_lines: same as pulse_timing.phase_2band_find
+    :return: None
+    """
+    band = str(band).lower()
+    cutnum = data.first_good_dataset.CUT_NAME.index('timing')
+    for ds in data:
+        band1, band2, bandNone = pulse_timing.phase_2band_find(ds.p_laser_phase,cut_lines=cut_lines)
+        if band == '1':
+            ds.cuts.cut(cutnum, np.logical_not(band1))
+        elif band == '2':
+            ds.cuts.cut(cutnum, np.logical_not(band2))
+        elif band == 'not_laser':
+            ds.cuts.cut(cutnum, np.logical_not(bandNone))
+        elif band == "laser":
+            ds.cuts.cut(cutnum, bandNone)
--- exafs_analysis.py
+++ exafs_analysis.py
@@ -17,6 +17,7 @@ noise_files = mass.ljh_chan_names(path.join(dir_base, dir_n), chan_nums)
 
 data = mass.TESGroup(pulse_files, noise_files)
 data.summarize_data_tdm(peak_time_microsec=220.0)
+exafs.calc_laser_phase(data)
 data.compute_noise_spectra()
 exafs.apply_cuts(data, exafs.basic_cuts)
 exafs.avg_pulse(data) # creates masks and compute average pulses
@@ -24,13 +25,28 @@ data.plot_average_pulses(-1)
 data.compute_filters(f_3db=10000.0)
 data.filter_data_tdm(forceNew=False)
 exafs.drift_correct(data)
+exafs.choose_laser(data, "not_laser")
+# exafs.phase_correct(data, typical_resolution=10)
 
-import pulse_timing
+
+
 ds = data.first_good_dataset
-pulse_timing.dataset_periodogram2(ds)
+d =good_pulses_data(ds)
 
 
-
-
-
-
+# import sklearn.cluster
+# res=10
+# MIN_PCT = 2
+# MIN_PULSES = 50
+# min_samples = max(MIN_PULSES, int(0.5+ 0.01*MIN_PCT*N))
+#
+# for ds in data:
+#     energy = ds.p_pulse_rms[ds.cuts.good()]
+#     N = len(energy)
+#
+#     _core_samples, labels = sklearn.cluster.dbscan(energy.reshape((N,1)), eps=res,
+#                                                    min_samples=min_samples)
+#     labels = np.asarray(labels, dtype=int)
+#     labelCounts,_ = np.histogram(labels, 1+labels.max(), [-.5, .5+labels.max()])
+#     print 'Label counts: ', labelCounts
+#
