216e75b69dc2e8b065624d74077fbdbba20a1109
Galen O'Neil <galen.oneil@nist.gov>
2014-08-05 18:34:11 -0600

    calc_laser_phase + hdf5 compatability

--- exafs_analysis.py
+++ exafs_analysis.py
@@ -11,8 +11,8 @@ import traceback, sys
 
 # load data
 dir_base = "/Volumes/Drobo/exafs_data"
-dir_p = "20140084_ferrioxalate_pump_probe/"
-dir_n = "20140084_ferrioxalate_pump_probe_noise/"
+dir_p = "20140724_ferrioxalate_pump_probe_100um_circ_2"
+dir_n = "20140724_ferrioxalate_pump_probe_100um_circ_noise"
 # dir_p = "20140617_laser_plus_calibronium_timing/"
 # dir_n = "20140617_laser_plus_calibronium_timing_noise/"
 available_chans = mass.ljh_get_channels_both(path.join(dir_base, dir_p), path.join(dir_base, dir_n))
@@ -33,13 +33,13 @@ data.plot_average_pulses(-1)
 data.compute_filters(f_3db=10000.0)
 data.filter_data(forceNew=False)
 pulse_timing.apply_offsets_for_monotonicity(data)
-pulse_timing.calc_laser_phase(data, forceNew=True)
+pulse_timing.calc_laser_phase(data, forceNew=False)
 pulse_timing.choose_laser(data, "not_laser")
-data.drift_correct(forceNew=True)
-data.phase_correct2014(10, plot=True)
+data.drift_correct(forceNew=False)
+data.phase_correct2014(10, plot=False)
 data.calibrate('p_filt_value_dc', ['VKAlpha', 'MnKAlpha', 'MnKBeta', 'FeKAlpha', 'CoKAlpha', 'CoKBeta', 'CuKAlpha', "FeKBeta", "VKBeta","CuKBeta","ScKAlpha","NiKAlpha"],
                         size_related_to_energy_resolution=20.0,min_counts_per_cluster=20,
-                        excl=[],forceNew=True, max_num_clusters = 18, plot_on_fail=True, max_pulses_for_dbscan=1e5)
+                        excl=[],forceNew=False, max_num_clusters = 18, plot_on_fail=True, max_pulses_for_dbscan=1e5)
 data.calibrate('p_filt_value_phc', ['VKAlpha', 'MnKAlpha', 'MnKBeta', 'FeKAlpha', 'CoKAlpha', 'CoKBeta', 'CuKAlpha', "FeKBeta", "VKBeta","CuKBeta","ScKAlpha","NiKAlpha"],
                         size_related_to_energy_resolution=20.0,min_counts_per_cluster=20,
                         excl=[],forceNew=False, max_num_clusters = 18, plot_on_fail=True, max_pulses_for_dbscan=1e5)
@@ -53,7 +53,7 @@ pulse_timing.label_pumped_band_for_alternating_pump(data, forceNew=False)
 pulse_timing.choose_laser(data, "laser")
 exafs.quality_control(data, exafs.edge_center_func, "FeKEdge Location")
 exafs.quality_control(data, exafs.chi2_func, "edge fit chi^2", threshold=8)
-exafs.fit_edge_in_energy_combined(data, "FeKAlpha", doPlot=True)
+exafs.fit_edge_in_energy_combined(data, "FeKEdge", doPlot=True)
 
 # # write histograms
 exafs.plot_combined_spectra(data, ref_lines=["FeKEdge"])
--- pulse_timing.py
+++ pulse_timing.py
@@ -180,7 +180,7 @@ def calc_phase(timestamp,f0=None,flatten=True,num_bands=2,f_guess_range=(1000,10
         return (timestamp*f0)%num_bands,f0, None
 
 def phase_2band_find(phase, cut_lines=[0.012,0.012], median=None):
-    if median is None: median = np.median(phase[0:np.amin(50000, len(phase))]%1)
+    if median is None: median = np.median(phase[0:min(50000, len(phase))]%1)
     a,b = median-cut_lines[0], median+cut_lines[1] # make cuts on either side of it
     band1 = np.logical_and(a<phase, phase<b)
     band2 = np.logical_and((a+1)<phase, phase<(b+1))
@@ -253,8 +253,8 @@ def calc_laser_phase(data, forceNew=False):
         print("skipping all of calc_laser_phase, all already done")
         return
     print("using spline from %d with %d pulses"%(ds.channum, ds.nPulses))
-    for ds in data:
-        if not hasattr(ds, "p_laser_phase") or forceNew:
+    for ds in data.iter_channels(include_badchan=True):
+        if not hasattr(ds, "p_laser_phase"):
             print("chan %d calculating laser phase"%ds.channum)
             ds.p_laser_phase = ds.hdf5_group.create_dataset("p_laser_phase", data=spline.phase(ds.p_timestamp[:]))
         else:
